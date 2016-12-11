5703b441760c3e224301fb3e5d2c57795793ec42
Andrey <patsko.andrey@gmail.com>
2016-12-11 19:04:12 +0300

    start making new function

--- mainFunctionNew.js
+++ mainFunctionNew.js
@@ -22,11 +22,33 @@ function categoriesCreate(array){
     db.categories.insertMany(categoriesArray,{"ordered":false,w:0});
 }
 
+//function get array of dates from rates collection
+function getArrayOfDates(){
+    var dateArrayNew=[];
+    var dateArray = db.rates.find({},{_id:0,"date":1}).toArray();
+    for (var t = 0; t < dateArray.length; t++){
+        dateArrayNew[t]=new Date(Date.parse(dateArray[t].date))
+    }
+    return dateArrayNew;
+}
+
 // main function
 function run(){
+    var beginDate,endDate;
+    var denominationDate = new Date(2016,06,01);
+//getting array of operations from collection operations
     var operationsArray = db.getCollection("operations").find({}).toArray();
+//createing collection of accounts and collection of categories
     accountCreate(operationsArray);
     categoriesCreate(operationsArray);
+//hetting array of dates from the collection rates and get beginDate and endDate   
+    var dateArray = getArrayOfDates();
+    var begin = dateArrayNew.sort(compareNumeric).slice(0,1)
+    var end = dateArrayNew.sort(compareNumeric).reverse().slice(0,1);
+    beginDate = begin[0];
+    endDate = end[0];
+
+
 }
 run();
 
