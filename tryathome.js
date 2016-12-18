73a627dec416bbc6e6516ee56837414fa9fe3c21
Andrey <patsko.andrey@gmail.com>
2016-12-18 20:34:33 +0300

    troubles with dates((

--- tryathome.js
+++ tryathome.js
@@ -7,6 +7,36 @@ function getArrayOfDates(){
     var dateArrayNew=[];
     var dateArray = db.rates.find({},{_id:0,"Date":1}).toArray();
     for (var t = 0; t < dateArray.length; t++){
+       var b = dateArray[t];
+        var a = b["Date"].split("/");
+        var c = new Date(a[2],a[0]-1,a[1],3,0,0,0)
+        var f = c.getHours();
+        print(f);
+    dateArrayNew.push(c)
+//         dateArrayNew[t]=new Date(Date.parse(dateArray[t].Date))
+//         dateArrayNew[t].setHours(2);// need thmsing do with dates
+   
+        }
+    return dateArrayNew;
+//         print(a)
+}
+
+var dateArray = getArrayOfDates();
+printjson(dateArray)
+//     var begin = dateArray.sort(compareNumeric).slice(0,1)
+//     var end = dateArray.sort(compareNumeric).reverse().slice(0,1); 
+//     var beginDate = begin[0] ;
+//     var endDate = end[0];
+
+function compareNumeric(a, b) {
+  if (a > b) return 1;
+  if (a < b) return -1;
+}
+//function get array of dates from rates collection
+function getArrayOfDates(){
+    var dateArrayNew=[];
+    var dateArray = db.rates.find({},{_id:0,"Date":1}).toArray();
+    for (var t = 0; t < dateArray.length; t++){
         dateArrayNew[t]=new Date(Date.parse(dateArray[t].Date))
         dateArrayNew[t].setHours(3);// need thmsing do with dates
     }
@@ -26,9 +56,10 @@ function create(){
     var beginDate = begin[0] ;
     var endDate = end[0];
     db.cashFlow.remove({});
-    db.transactions.createIndex({"Date":1});
+//     db.transactions.createIndex({"Date":1});
+
     // for(var i = 0; i < dateArray.length; i++){
-        while(beginDate < endDate){
+        while(beginDate <= endDate){
         var transactionsOfDateArray = db.getCollection('transactions').find({Date:beginDate}).toArray();
         for(var j = 0; j < transactionsOfDateArray.length; j++){
             if(transactionsOfDateArray[j]["Type"] == "Exp"){
